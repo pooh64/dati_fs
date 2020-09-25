2bf88f95bc99b4ad69825fa526ca85a27a120bae
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-09-25 20:13:39 +0300

    cmdline hider

---
+++ cmdline/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/prctl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	char *ptr = argv[0];
+	ptr += strlen(ptr) + 1;
+
+	if (prctl(PR_SET_MM, PR_SET_MM_ARG_START, ptr, 0, 0) < 0) {
+		perror("prctl");
+		exit(1);
+	}
+
+	printf("pid: %d\n", (int) getpid());
+	
+	sleep(100);
+	return 0;
+}
