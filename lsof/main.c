defa6a7e7cad8f53fdc7d717957acdf4fc548385
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-09-30 19:54:40 +0300

    Fix error handlers in ps, lsof

--- lsof/main.c
+++ lsof/main.c
@@ -8,13 +8,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <error.h>
+
+#define err_make(val) (-(val))
+#define err_display(errc, fmt, ...) error(0, (int) -(errc), fmt, ##__VA_ARGS__)
 
 /* buf size >= PATH_MAX required */
 static ssize_t read_link_str_at(int dir_fd, char *link_path, char *buf)
 {
 	ssize_t sz = readlinkat(dir_fd, link_path, buf, PATH_MAX);
 	if (sz < 0)
-		return -1;
+		return (ssize_t) err_make(errno);
 
 	buf[sz] = 0;
 	return sz;
@@ -22,9 +26,10 @@ static ssize_t read_link_str_at(int dir_fd, char *link_path, char *buf)
 
 static int handle_fd(int dir_fd, char *fd_str, char *path_buf)
 {
-	if (read_link_str_at(dir_fd, fd_str, path_buf) < 0) {
-		perror("read_link_str_at");
-		return -1;
+	int errc;
+	if ((errc = read_link_str_at(dir_fd, fd_str, path_buf)) < 0) {
+		err_display(errc, "read_link_str_at");
+		return errc;
 	}
 
 	printf("%5s -> %-64.64s\n", fd_str, path_buf);
@@ -33,24 +38,27 @@ static int handle_fd(int dir_fd, char *fd_str, char *path_buf)
 
 static int lsof_pid(char *pid_str)
 {
+	int errc;
 	char *path_buf = NULL;
 	DIR *dir = NULL;
 	int dir_fd;
 
 	if (!(path_buf = malloc(PATH_MAX))) {
-		perror("malloc");
-		goto handle_err;
+		errc = err_make(errno);
+		err_display(errc, "malloc");
+		goto out;
 	}
 
 	sprintf(path_buf, "/proc/%s/fd", pid_str);
 	if (!(dir = opendir(path_buf))) {
-		perror("opendir");
-		goto handle_err;
+		errc = err_make(errno);
+		err_display(errc, "opendir");
+		goto out;
 	}
-
 	if ((dir_fd = dirfd(dir)) < 0) {
-		perror("dirfd");
-		goto handle_err;
+		errc = err_make(errno);
+		err_display(errc, "dirfd");
+		goto out;
 	}
 
 	while (1) {
@@ -58,29 +66,32 @@ static int lsof_pid(char *pid_str)
 		struct dirent *ent = readdir(dir);
 		if (!ent) {
 			if (errno) {
-				perror("readdir");
-				goto handle_err;
+				errc = err_make(errno);
+				err_display(errc, "readdir");
+				goto out;
 			}
 			break;
 		}
 		if (ent->d_name[0] == '.')
 			continue;
-		if (handle_fd(dir_fd, ent->d_name, path_buf) < 0)
-			goto handle_err;
+		if ((errc = handle_fd(dir_fd, ent->d_name, path_buf)) < 0)
+			goto out;
 	}
 
 	closedir(dir);
 	free(path_buf);
 	return 0;
 
-handle_err:
-	if (path_buf)	free(path_buf);
+out:
+	free(path_buf);
 	if (dir)	closedir(dir);
-	return -1;
+	return errc;
 }
 
 int main(int argc, char **argv)
 {
+	int errc;
+
 	if (argc != 2) {
 		fprintf(stderr, "Usage: lsof pid\n");
 		exit(1);
@@ -93,8 +104,10 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	if (lsof_pid(argv[1]) < 0)
+	if ((errc = lsof_pid(argv[1])) < 0) {
+		err_display(errc, "fatal error");
 		exit(1);
+	}
 
 	return 0;
 }
--- ps/main.c
+++ ps/main.c
@@ -8,45 +8,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <error.h>
 
-/* openat FILE* in RDONLY mode
- * error: returns NULL, sets errno */
-static FILE *proc_fstream_openat(int dirfd, char const *path)
+#define err_make(val) (-(val))
+#define err_display(errc, fmt, ...) error(0, (int) -(errc), fmt, ##__VA_ARGS__)
+
+static int proc_fstream_openat(FILE **file, int dirfd, char const *path)
 {
 	int fd = openat(dirfd, path, O_RDONLY);
 	if (fd < 0)
-		return NULL;
+		return err_make(errno);
 
-	return fdopen(fd, "r");
+	if (!(*file = fdopen(fd, "r")))
+		return err_make(errno);
+	return 0;
 }
 
-/* full read for files with undetermined size
- * error: returns -1, sets errno */
+#define FSTREAM_GET_DATA_MAX (4096 * 4)
+
 static ssize_t fstream_get_data(FILE *f, char **buf)
 {
+	int errc;
 	char chunk[4096];
 	ssize_t nr = 0, rv;
 	void *tmp;
 	*buf = NULL;
 
 	while ((rv = fread(chunk, 1, sizeof(chunk), f)) > 0) {
+		if (nr + rv > FSTREAM_GET_DATA_MAX) {
+			errc = err_make(EFBIG);
+			goto out_free;
+		}
 		if (!(tmp = realloc(*buf, nr + rv))) {
-			goto handle_err;
+			errc = err_make(errno);
+			goto out_free;
 		}
 		*buf = tmp;
 		memcpy(*buf + nr, chunk, rv);
 		nr += rv;
 	}
 
-	if (ferror(f))
-		goto handle_err;
-
+	if (ferror(f)) {
+		errc = err_make(errno);
+		goto out_free;
+	}
 	return nr;
 
-handle_err:
-	if (buf)
-		free(buf);
-	return -1;
+out_free:
+	free(buf);
+	return (ssize_t) errc;
 }
 
 /* /proc/[pid]/stat */
@@ -59,33 +69,37 @@ struct proc_pid_stat {
 	char *comm;
 };
 
-/* error: returns -1, sets errno */
 static int proc_pid_stat_get(int pid_fd, struct proc_pid_stat *stat)
 {
-	FILE *file = proc_fstream_openat(pid_fd, "stat");
-	if (!file)
-		goto handle_err_0;
+	int errc;
+	FILE *file;
+	if ((errc = proc_fstream_openat(&file, pid_fd, "stat")) < 0)
+		goto out;
 
-	if (!(stat->comm = malloc(256)))
-		goto handle_err_1;
+	if (!(stat->comm = malloc(256))) {
+		errc = err_make(errno);
+		goto out_fclose;
+	}
 
 	errno = 0;
 	fscanf(file, "%d", &stat->pid);
 	fscanf(file, " (%[^)]s", stat->comm);
 	fscanf(file, ") %c", &stat->state);
 	fscanf(file, "%d %d %d", &stat->ppid, &stat->pgrp, &stat->sid);
-	if (errno)
-		goto handle_err_2;
+	if (errno) {
+		errc = err_make(errno);
+		goto out_free;
+	}
 
 	fclose(file);
 	return 0;
 
-handle_err_2:
+out_free:
 	free(stat->comm);
-handle_err_1:
+out_fclose:
 	fclose(file);
-handle_err_0:
-	return -1;
+out:
+	return errc;
 }
 
 static void proc_pid_stat_destroy(struct proc_pid_stat *stat)
@@ -95,13 +109,14 @@ static void proc_pid_stat_destroy(struct proc_pid_stat *stat)
 
 static int proc_pid_cmdline_get(int pid_fd, char **str, size_t *sz)
 {
-	FILE *file = proc_fstream_openat(pid_fd, "cmdline");
-	if (!file)
-		goto handle_err_0;
+	int errc;
+	FILE *file;
+	if ((errc = proc_fstream_openat(&file, pid_fd, "cmdline")) < 0)
+		goto out;
 
 	ssize_t rc = fstream_get_data(file, str);
-	if (rc < 0)
-		goto handle_err_1;
+	if ((errc = rc) < 0)
+		goto out_fclose;
 	*sz = rc;
 
 	for (ssize_t i = 0; i < rc - 1; ++i) {
@@ -112,10 +127,10 @@ static int proc_pid_cmdline_get(int pid_fd, char **str, size_t *sz)
 	fclose(file);
 	return 0;
 
-handle_err_1:
+out_fclose:
 	fclose(file);
-handle_err_0:
-	return -1;
+out:
+	return errc;
 }
 
 static int display_head()
@@ -127,24 +142,25 @@ static int display_head()
 
 static int display_pid(int pid_fd)
 {
+	int errc;
 	struct proc_pid_stat stat;
 	char *str;
 	size_t sz;
 
-	if (proc_pid_stat_get(pid_fd, &stat) < 0) {
-		perror("obtain /proc/pid/stat data");
-		return -1;
-	} else {
-		printf("%6d", stat.pid);
-		printf("  %-25.25s", stat.comm);
-		printf("  %6d  %6d  %6d", stat.ppid, stat.pgrp, stat.sid);
-		proc_pid_stat_destroy(&stat);
+	if ((errc = proc_pid_stat_get(pid_fd, &stat)) < 0) {
+		err_display(errc, "obtain /proc/pid/stat data");
+		return errc;
 	}
+	printf("%6d", stat.pid);
+	printf("  %-25.25s", stat.comm);
+	printf("  %6d  %6d  %6d", stat.ppid, stat.pgrp, stat.sid);
+	proc_pid_stat_destroy(&stat);
 
-	if (proc_pid_cmdline_get(pid_fd, &str, &sz) < 0) {
-		perror("obtain /proc/pid/cmdline");
-		return -1;
-	} else if (sz) {
+	if ((errc = proc_pid_cmdline_get(pid_fd, &str, &sz)) < 0) {
+		err_display(errc, "obtain /proc/pid/cmdline");
+		return errc;
+	}
+	if (sz) {
 		printf("  %-32.32s", str);
 		free(str);
 	}
@@ -155,31 +171,41 @@ static int display_pid(int pid_fd)
 
 static int handle_pid(int proc_fd, char *pid_str)
 {
+	int errc;
 	int pid_fd = openat(proc_fd, pid_str, O_RDONLY);
 	if (pid_fd < 0) {
-		perror("openat");
-		goto handle_err_0;
+		errc = err_make(errno);
+		err_display(errc, "openat");
+		goto out;
 	}
 
-	if (display_pid(pid_fd) < 0)
-		goto handle_err_1;
+	if ((errc = display_pid(pid_fd)) < 0)
+		goto out_close;
 
 	close(pid_fd);
 	return 0;
 
-handle_err_1:
+out_close:
 	close(pid_fd);
-handle_err_0:
-	return -1;
+out:
+	return errc;
 }
 
 static int proc_ps()
 {
-	DIR *proc_dir = opendir("/proc/");
-	int proc_fd = dirfd(proc_dir);
-	if (proc_fd < 0) {
-		perror("dirfd");
-		goto handle_err_0;
+	int errc;
+	DIR *proc_dir;
+	int proc_fd;
+	
+	if (!(proc_dir = opendir("/proc/"))) {
+		errc = err_make(errno);
+		err_display(errc, "opendir");
+		goto out;
+	}
+	if ((proc_fd = dirfd(proc_dir)) < 0) {
+		errc = err_make(errno);
+		err_display(errc, "dirfd");
+		goto out_closedir;
 	}
 	display_head();
 
@@ -188,8 +214,9 @@ static int proc_ps()
 		struct dirent *ent = readdir(proc_dir);
 		if (!ent) {
 			if (errno) {
-				perror("readdir");
-				goto handle_err_1;
+				errc = err_make(errno);
+				err_display(errc, "readdir");
+				goto out_closedir;
 			}
 			break;
 		}
@@ -201,22 +228,23 @@ static int proc_ps()
 		if (*eptr || errno)
 			continue;
 
-		if (handle_pid(proc_fd, ent->d_name) < 0)
-			goto handle_err_1;
+		if ((errc = handle_pid(proc_fd, ent->d_name)) < 0)
+			goto out_closedir;
 	}
 	closedir(proc_dir);
 	return 0;
 
-handle_err_1:
+out_closedir:
 	closedir(proc_dir);
-handle_err_0:
-	return -1;
+out:
+	return errc;
 }
 
 int main(int argc, char **argv)
 {
-	if (proc_ps() < 0) {
-		fprintf(stderr, "%s failed", argv[0]);
+	int errc;
+	if ((errc = proc_ps()) < 0) {
+		err_display(errc, "fatal error");
 		exit(1);
 	}
 	return 0;
